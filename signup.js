71c60b8e5e67416cd40d2a43c64b68af8cb0b3e5
IshanArora21 <34289247+IshanArora21@users.noreply.github.com>
2018-04-09 09:04:00 -0400

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,2 +1,4 @@
 //java script for sign up
 //we are probably going to use parse jQuery in order to create new user instances if using java script
+//use get data by  documet.get for id for user inputs
+//email verification?
